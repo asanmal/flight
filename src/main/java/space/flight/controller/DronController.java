929ca9d8add168fc134cfa4d420332d10850308c
asanmal <148326376+asanmal@users.noreply.github.com>
2025-03-21 13:59:46 +0100

    Añadidas clases controladoras para gestionar los servicios

--- src/main/java/space/flight/controller/DronController.java
+++ src/main/java/space/flight/controller/DronController.java
@@ -1,4 +1,27 @@
 package space.flight.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import space.flight.entity.Dron;
+import space.flight.service.DronService;
+
+@RestController
+@RequestMapping("/api/vuelos/dron")
+@RequiredArgsConstructor
 public class DronController {
+
+    private final DronService dronService;
+
+    // Crear un dron en el sistema
+    @PostMapping("/create")
+    public ResponseEntity<Dron> createDron(@RequestBody Dron dron) {
+        Dron savedDron = dronService.createDron(dron);
+        return new ResponseEntity<>(savedDron, HttpStatus.CREATED);
+    }
+
 }
