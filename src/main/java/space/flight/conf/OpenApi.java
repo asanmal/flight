fffb1ef27cd7982c9011d86537fdfb6b8019f5ff
asanmal <148326376+asanmal@users.noreply.github.com>
2025-03-24 07:27:27 +0100

    Configuración de OpenAPI con Swagger y ajustes en application.properties

---
+++ src/main/java/space/flight/conf/OpenApi.java
@@ -0,0 +1,25 @@
+package space.flight.conf;
+
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApi {
+
+    @Bean
+    public OpenAPI dronesOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Drones API")
+                        .description("API para gestionar drones y su matriz")
+                        .version("v1.0.0")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("Documentación de OpenAPI")
+                        .url("https://swagger.io/docs/"));
+    }
+}
